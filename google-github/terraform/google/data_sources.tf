1023df7c4d27781b451867122a8fd1d8e0252f9f
Won Kim <wkim@qualcomm.com>
2025-01-03 22:57:29 -0800

    changed editor to owner

--- google-github/terraform/google/data_sources.tf
+++ google-github/terraform/google/data_sources.tf
@@ -17,7 +17,7 @@ data "google_iam_role" "dns_admin" {
 }
 
 data "google_iam_role" "owner" {
-  name = "roles/editor"
+  name = "roles/owner"
 }
 
 data "google_iam_role" "secretmanager_secretaccessor" {
